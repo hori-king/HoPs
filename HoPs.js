3ab805970bbb2825f3dfbaf920b056be061a71b1
hori-king <kuhuangqi758@gmail.com>
2024-11-13 11:14:19 +0900

    Update HoPs.js

--- HoPs.js
+++ HoPs.js
@@ -19,3 +19,23 @@ document.querySelector('.gray_display').addEventListener('click',function(){
     this.classList.toggle('active'); //グレーのところ押したら
     document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
 });
+
+//ABOUT.htmlのときにリスト内のABOUTを押したとき
+document.querySelector('.about_list').addEventListener('click',function(){
+    hamburger.classList.toggle('active'); //ハンバーガーに戻る
+    list.classList.toggle('active'); //リストを閉じる
+
+    if(window.matchMedia('(max-width: 650px)').matches){
+        gray.classList.toggle('active'); //背景の色を変える
+    }else{};
+});
+
+//NEWS.htmlのときにリスト内のNEWSを押したとき
+document.querySelector('.news_list').addEventListener('click',function(){
+    hamburger.classList.toggle('active'); //ハンバーガーに戻る
+    list.classList.toggle('active'); //リストを閉じる
+
+    if(window.matchMedia('(max-width: 650px)').matches){
+        gray.classList.toggle('active'); //背景の色を変える
+    }else{};
+});
