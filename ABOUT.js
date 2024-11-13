3bb164fe36d56117b7f0b5e3e2a4622e27a70668
hori-king <kuhuangqi758@gmail.com>
2024-11-13 13:11:21 +0900

    Update ABOUT.js

--- ABOUT.js
+++ ABOUT.js
@@ -1,3 +1,31 @@
+//リスト内のOTHERを押したとき
+document.querySelector('.other_list').addEventListener('click',function(){
+    document.querySelector('.gray_display').classList.toggle('active'); //背景の色を変える
+
+    //横幅が650pxより大きいときOTHERを押すと
+    if(window.matchMedia('(min-width: 650px)').matches){
+        document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
+    }
+});
+
+// バツ印を押したとき
+document.querySelector('.other_batu').addEventListener('click',function(){
+    document.querySelector('.active').classList.toggle('active'); //リストを閉じる 
+    document.querySelector('.gray_display').classList.toggle('active');
+});
+
+//ハンバーガメニュー、リスト以外の場所を押したとき閉じる
+document.querySelector('.gray_display').addEventListener('click',function(){
+    this.classList.toggle('active'); //グレーのところ押したら
+    document.querySelector('.hamburger').classList.toggle('active'); //ハンバーガーに戻る 
+    
+    if(window.matchMedia('(min-width: 650px)').matches){
+        document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
+    }else{
+        document.querySelector('.active').classList.toggle('active'); //リストを閉じる
+    }
+});
+
 //ABOUT.htmlのときにリスト内のABOUTを押したとき
 document.querySelector('.about_list').addEventListener('click',function(){
     document.querySelector('.hamburger').classList.toggle('active'); //ハンバーガーに戻る
