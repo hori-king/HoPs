449afd511f382d2b1ed201e4290bd1d125e1d9ab
hori-king <kuhuangqi758@gmail.com>
2024-11-13 11:52:59 +0900

    Update HoPs.js

--- HoPs.js
+++ HoPs.js
@@ -5,7 +5,7 @@ document.querySelector('.other_list').addEventListener('click',function(){
     //横幅が650pxより大きいときOTHERを押すと
     if(window.matchMedia('(min-width: 650px)').matches){
         document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
-    }else{};  
+    }
 });
 
 // バツ印を押したとき
@@ -17,25 +17,9 @@ document.querySelector('.other_batu').addEventListener('click',function(){
 //ハンバーガメニュー、リスト以外の場所を押したとき閉じる
 document.querySelector('.gray_display').addEventListener('click',function(){
     this.classList.toggle('active'); //グレーのところ押したら
-    document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
-});
-
-//ABOUT.htmlのときにリスト内のABOUTを押したとき
-document.querySelector('.about_list').addEventListener('click',function(){
-    hamburger.classList.toggle('active'); //ハンバーガーに戻る
-    list.classList.toggle('active'); //リストを閉じる
-
-    if(window.matchMedia('(max-width: 650px)').matches){
-        gray.classList.toggle('active'); //背景の色を変える
-    }else{};
-});
-
-//NEWS.htmlのときにリスト内のNEWSを押したとき
-document.querySelector('.news_list').addEventListener('click',function(){
-    hamburger.classList.toggle('active'); //ハンバーガーに戻る
-    list.classList.toggle('active'); //リストを閉じる
-
-    if(window.matchMedia('(max-width: 650px)').matches){
-        gray.classList.toggle('active'); //背景の色を変える
-    }else{};
+    if(window.matchMedia('(min-width: 650px)').matches){
+        document.querySelector('.OTHER_list').classList.toggle('active'); //リストが出てくる
+    }else{
+        document.querySelector('.active').classList.toggle('active'); //リストを閉じる
+    }
 });
